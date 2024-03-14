bc6835a317d79396ce580b68928f5195ad84ecb2
Eyrxile <91099446+eyrxile@users.noreply.github.com>
2024-03-14 15:02:23 -0300

    Add files via upload

---
+++ Palindrome Number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x<0 || (x % 10 === 0 && x !== 0)){
+        return false;
+    }
+
+    let reversedNumber = 0;
+    let originalNumber = x;
+
+    while(x > 0){
+        const digit = x % 10;
+        reversedNumber = reversedNumber * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    return originalNumber === reversedNumber;
+};
+
+//Example usage:
+console.log(isPalindrome(121));
+console.log(isPalindrome(-121));
+console.log(isPalindrome(10));
